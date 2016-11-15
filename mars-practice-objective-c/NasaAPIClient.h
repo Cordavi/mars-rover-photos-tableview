541f6c6277342e7934af56cf760632a685fb9720
Cordavi <michaelamundsen@gmail.com>
2016-11-15 15:53:21 -0500

    Added NasaAPIClient and declared fetchPhotosJsonWithCompletion

--- mars-practice-objective-c/NasaAPIClient.h
+++ mars-practice-objective-c/NasaAPIClient.h
@@ -1,13 +1,7 @@
-//
-//  NasaAPIClient.h
-//  mars-practice-objective-c
-//
-//  Created by Michael Amundsen on 11/15/16.
-//  Copyright © 2016 Michael Amundsen. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface NasaAPIClient : NSObject
+
+-(void)fetchPhotosJSONWithCompletion:(void(^)(NSDictionary *))completion;
 
 @end
--- mars-practice-objective-c/NasaAPIClient.m
+++ mars-practice-objective-c/NasaAPIClient.m
@@ -1,13 +1,9 @@
-//
-//  NasaAPIClient.m
-//  mars-practice-objective-c
-//
-//  Created by Michael Amundsen on 11/15/16.
-//  Copyright © 2016 Michael Amundsen. All rights reserved.
-//
-
 #import "NasaAPIClient.h"
 
 @implementation NasaAPIClient
+
+-(void)fetchPhotosJSONWithCompletion:(void(^)(NSDictionary *))completion {
+  
+}
 
 @end
