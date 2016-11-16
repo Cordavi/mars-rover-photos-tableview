ebb9d645304663d4237de21862463edcb494b6df
Cordavi <michaelamundsen@gmail.com>
2016-11-16 12:32:32 -0500

    Set up implementation of retreving photosJson

--- mars-practice-objective-c/ContentManager.m
+++ mars-practice-objective-c/ContentManager.m
@@ -1,13 +1,51 @@
-//
-//  ContentManager.m
-//  mars-practice-objective-c
-//
-//  Created by Michael Amundsen on 11/16/16.
-//  Copyright © 2016 Michael Amundsen. All rights reserved.
-//
-
 #import "ContentManager.h"
+#import "NasaAPIClient.h"
+#import "NasaPhoto.h"
+
+@interface ContentManager()
+
+@property (strong, nonatomic) NSMutableArray *photosJsonArray;
+@property (strong, nonatomic) NSMutableDictionary *photosDictionary;
+@property (strong, nonatomic) NSMutableDictionary *photosRequests;
+@property (strong, nonatomic) NasaAPIClient *apiClient;
+
+@end
+
 
 @implementation ContentManager
 
+-(void)fetchPhotosJson {
+  __weak ContentManager *weakSelf = self;
+  
+  [self.apiClient fetchPhotosJsonWithCompletion:^(NSDictionary * _Nullable completion) {
+    if (!completion) {
+      return;
+    }
+    
+    NSError *jsonError;
+    NSArray *photosJsonArray = [NSJSONSerialization JSONObjectWithData:completion[@"photos"] options:0 error:&jsonError];
+    if (!photosJsonArray) {
+      return;
+    }
+    
+    for (NSDictionary *photosJsonDictionary in photosJsonArray) {
+      NasaPhoto *newPhoto = [[NasaPhoto alloc] initWithPhotoJson:photosJsonDictionary];
+      if (newPhoto) {
+        [weakSelf.photosJsonArray addObject:newPhoto];
+      }
+    }
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+      //call delegate
+    });
+    
+  }];
+  
+}
+
+
 @end
+
+//fetch photo json
+
+//fetch image data
