e160c1e943436283e3e4ce0f5d9da4bc445be204
Cordavi <michaelamundsen@gmail.com>
2016-11-16 17:03:35 -0500

    Added initalize, delegate methods, and fixed photoJson array inialization

--- mars-practice-objective-c/ContentManager.m
+++ mars-practice-objective-c/ContentManager.m
@@ -2,17 +2,35 @@
 #import "NasaAPIClient.h"
 #import "NasaPhoto.h"
 
+
 @interface ContentManager()
 
-@property (strong, nonatomic) NSMutableArray *photosJsonArray;
-@property (strong, nonatomic) NSMutableDictionary *photosDictionary;
-@property (strong, nonatomic) NSMutableDictionary *photosRequests;
 @property (strong, nonatomic) NasaAPIClient *apiClient;
 
 @end
 
-
 @implementation ContentManager
+
+@synthesize delegate;
+
+- (instancetype)init
+{
+  self = [super init];
+  if (self) {
+    _apiClient = [[NasaAPIClient alloc] init];
+    _photosJsonArray = [[NSMutableArray alloc] init];
+  }
+  return self;
+}
+
+
+-(id)delegate {
+  return delegate;
+}
+
+-(void)setDelegate:(id<ContentManagerDelegate>)newDelegate {
+  delegate = newDelegate;
+}
 
 -(void)fetchPhotosJson {
   __weak ContentManager *weakSelf = self;
@@ -22,8 +40,8 @@
       return;
     }
     
-    NSError *jsonError;
-    NSArray *photosJsonArray = [NSJSONSerialization JSONObjectWithData:completion[@"photos"] options:0 error:&jsonError];
+//    NSError *jsonError;
+    NSArray *photosJsonArray = completion[@"photos"];
     if (!photosJsonArray) {
       return;
     }
@@ -36,16 +54,14 @@
     }
     
     dispatch_async(dispatch_get_main_queue(), ^{
-      //call delegate
+      [delegate photoJsonDidLoad:weakSelf];
     });
     
   }];
-  
 }
 
 
 @end
 
-//fetch photo json
 
 //fetch image data
