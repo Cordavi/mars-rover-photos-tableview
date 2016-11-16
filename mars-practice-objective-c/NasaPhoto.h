041b25e6e09b456571a34e3b77a2ea4907408961
Cordavi <michaelamundsen@gmail.com>
2016-11-16 12:01:35 -0500

    Inital commit of NasaPhoto class

---
+++ mars-practice-objective-c/NasaPhoto.h
@@ -0,0 +1,13 @@
+//
+//  NasaPhoto.h
+//  mars-practice-objective-c
+//
+//  Created by Michael Amundsen on 11/16/16.
+//  Copyright © 2016 Michael Amundsen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NasaPhoto : NSObject
+
+@end
