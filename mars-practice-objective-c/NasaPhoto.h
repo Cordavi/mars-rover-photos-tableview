dba2f1663bc0a7cfbb1fbd569896ebdbd20356ed
Cordavi <michaelamundsen@gmail.com>
2016-11-16 12:02:14 -0500

    Added properties and convience init for NasaPhoto class

--- mars-practice-objective-c/NasaPhoto.h
+++ mars-practice-objective-c/NasaPhoto.h
@@ -1,12 +1,5 @@
-//
-//  NasaPhoto.h
-//  mars-practice-objective-c
-//
-//  Created by Michael Amundsen on 11/16/16.
-//  Copyright © 2016 Michael Amundsen. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface NasaPhoto : NSObject
 
--- mars-practice-objective-c/NasaPhoto.m
+++ mars-practice-objective-c/NasaPhoto.m
@@ -1,13 +1,34 @@
-//
-//  NasaPhoto.m
-//  mars-practice-objective-c
-//
-//  Created by Michael Amundsen on 11/16/16.
-//  Copyright © 2016 Michael Amundsen. All rights reserved.
-//
-
 #import "NasaPhoto.h"
 
+@interface NasaPhoto()
+
+@property (strong, nonatomic) NSURL *_Nonnull imageURL;
+@property (strong, nonatomic) NSDate *_Nullable created;
+
+@end
+
 @implementation NasaPhoto
+
+- (instancetype)initWithPhotoJson:(NSDictionary *)photosJson {
+  self = [super init];
+  if (self) {
+    NSString *imageURLString = [NSString stringWithFormat:@"%@", photosJson[@"img_src"]];
+    NSURL *imageURL = [NSURL URLWithString: imageURLString];
+    if (!imageURL) {
+      return self;
+    }
+    _imageURL = imageURL;
+    
+    NSString *dateString = [NSString stringWithFormat:@"%@", photosJson[@"earth_date"]];
+    NSDateFormatter *earthDateFormatter = [[NSDateFormatter alloc] init];
+    [earthDateFormatter setDateFormat:@"yyyy-MM-dd"];
+    NSDate *created = [earthDateFormatter dateFromString:dateString];
+    if (!created) {
+      _created = nil;
+    }
+    _created = created;
+  }
+  return self;
+}
 
 @end
