f82222b22404b2e8418bc3a8332fbc0961fc4230
Cordavi <michaelamundsen@gmail.com>
2016-11-16 11:12:13 -0500

    Inital commit of ContentManager class

---
+++ mars-practice-objective-c/ContentManager.h
@@ -0,0 +1,13 @@
+//
+//  ContentManager.h
+//  mars-practice-objective-c
+//
+//  Created by Michael Amundsen on 11/16/16.
+//  Copyright © 2016 Michael Amundsen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContentManager : NSObject
+
+@end
---
+++ mars-practice-objective-c/ContentManager.m
@@ -0,0 +1,13 @@
+//
+//  ContentManager.m
+//  mars-practice-objective-c
+//
+//  Created by Michael Amundsen on 11/16/16.
+//  Copyright © 2016 Michael Amundsen. All rights reserved.
+//
+
+#import "ContentManager.h"
+
+@implementation ContentManager
+
+@end
