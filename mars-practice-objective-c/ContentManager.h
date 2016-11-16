82fda79e473490496a66f8a7c4e9be408e486301
Cordavi <michaelamundsen@gmail.com>
2016-11-16 17:02:40 -0500

    Added ContentManagerDelegate protocol. Exposed data needed for tableView

--- mars-practice-objective-c/ContentManager.h
+++ mars-practice-objective-c/ContentManager.h
@@ -1,8 +1,24 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+@class ContentManager;
+
+@protocol ContentManagerDelegate <NSObject>
+
+@required
+-(void)photoJsonDidLoad:(ContentManager *)contentManagerDelegate;
+
+@end
+
 @interface ContentManager : NSObject
 
+@property (nonatomic, weak) id <ContentManagerDelegate> delegate;
+@property (strong, nonatomic) NSMutableArray *photosJsonArray;
+@property (strong, nonatomic) NSMutableDictionary *photosDictionary;
+@property (strong, nonatomic) NSMutableDictionary *photosRequests;
+
+-(id)delegate;
+-(void)setDelegate:(id<ContentManagerDelegate>)newDelegate;
 -(void)fetchPhotosJson;
 -(void)fetchImageForIndexPath:(NSIndexPath *)indexPath;
 -(UIImage *)imageForIndexPath:(NSIndexPath *)indexPath;
