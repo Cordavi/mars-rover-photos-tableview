e52dce825c9b5af8803daa934b3ba505ccb9cad0
Cordavi <michaelamundsen@gmail.com>
2016-11-17 13:41:17 -0500

    Inital commit of new files

---
+++ mars-practice-objective-c/NasaPhotoTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NasaPhotoTableViewCell.h
+//  mars-practice-objective-c
+//
+//  Created by Michael Amundsen on 11/17/16.
+//  Copyright © 2016 Michael Amundsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NasaPhotoTableViewCell : UITableViewCell
+
+@end
