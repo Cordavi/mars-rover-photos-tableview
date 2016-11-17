1bb56fbf1afc7f47ddafdc699f04e52cb5be6a47
Cordavi <michaelamundsen@gmail.com>
2016-11-17 10:39:02 -0500

    Set cell text to camera name and detail text to rover name

--- mars-practice-objective-c/NasaTableViewController.m
+++ mars-practice-objective-c/NasaTableViewController.m
@@ -8,6 +8,7 @@
 @end
 
 @implementation NasaTableViewController
+@dynamic tableView;
 
 - (void)viewDidLoad {
   [super viewDidLoad];
@@ -34,59 +35,15 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
   UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"NasaTableViewCell" forIndexPath:indexPath];
   
-  NasaPhoto *cellPhoto = self.contentManager.photosJsonArray[indexPath.row];
-
-  cell.detailTextLabel.text = [NSString stringWithFormat:@"%@", [cellPhoto createdDate]];
+  NasaPhoto *nasaData = self.contentManager.photosJsonArray[indexPath.row];
+  cell.textLabel.text = [nasaData cameraName];
+  cell.detailTextLabel.text = [nasaData roverName];
   
   return cell;
 }
 
 - (void)photoJsonDidLoad:(ContentManager *)contentManagerDelegate {
-  self.tableView.reloadData;
+  [[self tableView] reloadData];
 }
-
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- } else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
 
 @end
