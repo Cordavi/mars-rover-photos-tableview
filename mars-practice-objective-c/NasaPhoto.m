c2f2c50645049feb78f1333aa396b7e71e2f08cc
Cordavi <michaelamundsen@gmail.com>
2016-11-17 10:38:35 -0500

    Added camera and rover properties, modified init to populate these fields

--- mars-practice-objective-c/NasaPhoto.m
+++ mars-practice-objective-c/NasaPhoto.m
@@ -2,6 +2,8 @@
 
 @interface NasaPhoto()
 
+@property (strong, nonatomic) NSString *_Nullable camera;
+@property (strong, nonatomic) NSString *_Nullable rover;
 @property (strong, nonatomic) NSURL *_Nonnull imageURL;
 @property (strong, nonatomic) NSDate *_Nullable created;
 
@@ -12,6 +14,18 @@
 -(instancetype)initWithPhotoJson:(NSDictionary *)photosJson {
   self = [super init];
   if (self) {
+    NSString *cameraName = [NSString stringWithFormat:@"%@", photosJson[@"camera"][@"name"]];
+    if (!cameraName) {
+      _camera = nil;
+    }
+    _camera = cameraName;
+    
+    NSString *roverName = [NSString stringWithFormat:@"%@", photosJson[@"rover"][@"name"]];
+    if (!roverName) {
+      _rover = nil;
+    }
+    _rover = roverName;
+    
     NSString *imageURLString = [NSString stringWithFormat:@"%@", photosJson[@"img_src"]];
     NSURL *imageURL = [NSURL URLWithString: imageURLString];
     if (!imageURL) {
@@ -37,6 +51,14 @@
 
 -(NSDate *)createdDate {
   return self.created;
+}
+
+-(NSString *)roverName {
+  return self.rover;
+}
+
+-(NSString *)cameraName {
+  return self.camera;
 }
 
 @end
