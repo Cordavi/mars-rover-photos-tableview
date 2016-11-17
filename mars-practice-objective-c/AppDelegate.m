bbbf028b9a016d05d9f1466fed09da967fb46fc1
Cordavi <michaelamundsen@gmail.com>
2016-11-17 13:47:32 -0500

    Updating project file and removed auto generated default text in files

--- mars-practice-objective-c/AppDelegate.m
+++ mars-practice-objective-c/AppDelegate.m
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.m
-//  mars-practice-objective-c
-//
-//  Created by Michael Amundsen on 11/15/16.
-//  Copyright © 2016 Michael Amundsen. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
--- mars-practice-objective-c/NasaTableViewController.h
+++ mars-practice-objective-c/NasaTableViewController.h
@@ -1,11 +1,3 @@
-//
-//  NasaTableViewController.h
-//  mars-practice-objective-c
-//
-//  Created by Michael Amundsen on 11/16/16.
-//  Copyright © 2016 Michael Amundsen. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface NasaTableViewController : UITableViewController
